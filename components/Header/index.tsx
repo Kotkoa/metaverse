c2af878f26ab493abcf2b939319f505b4798f46d
dirtyhandz <antonrozanov@gmail.com>
2022-06-19 12:47:48 +0300

    first

---
+++ components/Header/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+
+const Header = () => {
+    return (
+        <div className="bg-[2B2E35] flex justify-between">
+            Header
+        </div>
+    )
+}
+
+export default Header
