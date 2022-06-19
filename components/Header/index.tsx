67fdbed3664fd7e29cffbef188e8fd4dc6853cb9
Kotkoa <kotkoa@gmail.com>
2022-06-19 16:30:17 +0200

    logo-img fix

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
-import {Logo} from "../Logo/index";
 
 export default function Header() {
-    return (
-        <div className="flex justify-between items-center bg-[#2B2E35] p-6">
-            <div>
-                <Logo className="p-28"/>
-            </div>
-            <div></div>
-        </div>
-    )
+  return (
+    <div className="flex items-center justify-between bg-[#2B2E35] py-6 px-[120px] text-white">
+      <img src="svg/logo.svg" alt="LOGO" />
+      <div className="p-0">Button</div>
+    </div>
+  )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,21 @@
 // import Head from 'next/head'
 
-import Header from '../components/Header/index';
+import Header from '../components/Header/index'
 
 function HomePage() {
-    return (
-        <div>
-            <head>
-                <meta charSet="UTF-8"/>
-                <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-                <title>Document</title>
-            </head>
-            <main className="bg-[#212429] w-full min-h-screen flex flex-col">
-                <Header/>
-            </main>
-        </div>
-    )
+  return (
+    <div>
+      <head>
+        <meta charSet="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Metaverse</title>
+      </head>
+      <main className="flex min-h-screen w-full flex-col bg-[#212429]">
+        <Header />
+      </main>
+    </div>
+  )
 }
 
 export default HomePage
