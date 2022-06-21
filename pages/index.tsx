f764519b036d371f1bb402c0bb7cb779eadd0f24
Kotkoa <kotkoa@gmail.com>
2022-06-21 10:56:37 +0200

    make it a little bit responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,16 +8,16 @@ const Home: NextPage = () => {
       <div className="mx-auto max-w-[1440px]">
         <Header />
         <main>
-          <section className="flex h-[934px]">
+          <section className="flex h-auto min-h-[934px]">
             <div className="mt-[217px] w-1/2 pr-[82px] pl-[120px]">
               <h1 className=" text-[70px] font-bold uppercase tracking-widester text-white text-opacity-90">
                 Start your game jorney here
               </h1>
               <p className="mt-[30px] text-lg font-normal leading-[30px] text-white text-opacity-60">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis et tortor egestas mi curabitur varius
-                dignissim viverra.{' '}
+                dignissim viverra.
               </p>
-              <button className="mt-[44px] rounded-[28px] bg-gradient-to-r from-[#FD6D1D] to-[#E53AD2] px-12 py-2.5 tracking-widestmore text-white text-opacity-90">
+              <button className="my-[44px] rounded-[28px] bg-gradient-to-r from-[#FD6D1D] to-[#E53AD2] px-12 py-2.5 tracking-widestmore text-white text-opacity-90">
                 Explore
               </button>
             </div>
