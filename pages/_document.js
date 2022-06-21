c4206efeef7fd78151d5b59fa5aa5e26fb512571
Kotkoa <kotkoa@gmail.com>
2022-06-21 10:11:23 +0200

    fix fonts - 2 variants to add

--- pages/_document.js
+++ pages/_document.js
@@ -1,25 +1,28 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-    render() {
-        return (
-            <Html>
-                <Head>
-                    <link
-                        href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
-                        rel="stylesheet"
-                    />
-                    {/*<link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin=""/>*/}
-                    {/*    <link rel="preconnect" href="https://fonts.gstatic.com"/>*/}
-                    {/*        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"/>*/}
-                </Head>
-                <body>
-                <Main />
-                <NextScript />
-                </body>
-            </Html>
-        )
-    }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>Metaverse</title>
+          <meta name="description" content="Metaverse" />
+          <link rel="icon" href="/favicon.ico" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter&display=optional" rel="stylesheet" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body className="font-poppins">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
 
 export default MyDocument
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,34 @@
-import type {NextPage} from 'next'
+import type { NextPage } from 'next'
 import Head from 'next/head'
-// import Image from 'next/image'
-// import styles from '../styles/Home.module.css'
-import {Header} from "../components/Header";
+import { Header } from '../components/Header'
 
 const Home: NextPage = () => {
-    return (
-        <div className="bg-[#212429] min-h-screen w-full font-poppins">
-            <Head>
-                <title>Metaverse</title>
-                <meta name="description" content="Metaverse"/>
-                <link rel="icon" href="/favicon.ico"/>
-            </Head>
-            <div className="mx-auto max-w-[1440px]">
-                <Header/>
-                <main className="">
-                    <section className="flex h-[934px]">
-                        <div className="flex-1 mt-[217px] pr-[82px] pl-[120px]">
-                            <h1 className="text-white text-opacity-90 font-bold text-[70px] uppercase">Start your game jorney here</h1>
-                            <p className="text-white text-opacity-60 mt-[30px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis et tortor egestas mi curabitur varius dignissim viverra. </p>
-                            <button className="rounded-[28px] bg-gradient-to-r from-[#FD6D1D] to-[#E53AD2] px-6 py-2.5 text-white text-opacity-90 mt-[44px]">Explore</button>
-                        </div>
-                        <div className="flex-1">
-                            <img src="/png/first_page.png" alt="" className="w-full"/>
-                        </div>
-                    </section>
-
-                </main>
-
+  return (
+    <div className="min-h-screen w-full bg-[#212429]">
+      <div className="mx-auto max-w-[1440px]">
+        <Header />
+        <main className="">
+          <section className="flex h-[934px]">
+            <div className="mt-[217px] flex-1 pr-[82px] pl-[120px]">
+              <h1 className="text-[70px] font-bold uppercase text-white text-opacity-90">
+                Start your game jorney here
+              </h1>
+              <p className="mt-[30px] text-white text-opacity-60">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis et tortor egestas mi curabitur varius
+                dignissim viverra.{' '}
+              </p>
+              <button className="mt-[44px] rounded-[28px] bg-gradient-to-r from-[#FD6D1D] to-[#E53AD2] px-6 py-2.5 text-white text-opacity-90">
+                Explore
+              </button>
             </div>
-
-        </div>
-    )
+            <div className="flex-1">
+              <img src="/png/first_page.png" alt="" className="w-full" />
+            </div>
+          </section>
+        </main>
+      </div>
+    </div>
+  )
 }
 
 export default Home
