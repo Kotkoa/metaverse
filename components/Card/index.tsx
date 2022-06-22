b7609d17e22a44f1077efacf044bfb427707bcae
Kotkoa <kotkoa@gmail.com>
2022-06-22 12:00:55 +0200

    update svgs

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image'
 import cardImage from '../../public/png/Cards/img.png'
+import Clock from '../../public/svg/Card/clock.svg'
+import Heart from '../../public/svg/Card/heart.svg'
+import Etherium from '../../public/svg/Card/ethereum.svg'
 
 const Card = ({ name, highestBid, price, time, like }) => {
   return (
@@ -7,13 +10,13 @@ const Card = ({ name, highestBid, price, time, like }) => {
       <div className="relative">
         <Image src={cardImage} alt="cardImage" layout="responsive" />
         <div className="absolute top-[10px] left-[10px] flex content-center rounded-[20px] bg-[#2B2E35] py-2.5 pl-[14px] pr-[18px]">
-          <img src="svg/Card/clock.svg" alt="clock" />
+          <Clock />
           <p className="ml-[10px] bg-gradient-to-br from-[#FD6D1D] to-[#E53AD2] bg-clip-text text-[14px] tracking-[0.05em] text-transparent">
             08:10:00
           </p>
         </div>
         <div className="absolute top-[10px] right-[10px] rounded-[20px] bg-[#2B2E35] p-2.5">
-          <img src="svg/Card/heart.svg" alt="heart" />
+          <Heart />
         </div>
       </div>
       <div className="flex justify-between mt-5">
@@ -21,7 +24,7 @@ const Card = ({ name, highestBid, price, time, like }) => {
           <h1 className="text-xl text-white opacity-90">{name}</h1>
           <p className="mt-2 text-sm text-white opacity-60">{highestBid}</p>
         </div>
-        <img src="svg/Card/ethereum.svg" alt="ethereum" />
+        <Etherium />
       </div>
       <div className="flex justify-between mt-5">
         <p className="text-base text-white opacity-90">Price</p>
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+//import Image from 'next/image'
 import Link from 'next/link'
 import Logo from '/public/svg/Header/logo.svg'
 
