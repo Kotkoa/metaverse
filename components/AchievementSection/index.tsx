7e87fcb6677ca1f1ab9219a79b83ad237c6e718a
Kotkoa <kotkoa@gmail.com>
2022-06-22 09:32:20 +0200

    prettier

--- components/AchievementSection/index.tsx
+++ components/AchievementSection/index.tsx
@@ -1,15 +1,23 @@
-import {Card} from "../Card";
+import { Card } from '../Card'
 
-const AchievementSection = ({title, achievementCards}) => {
-    return (
-        <section className="mt-20 px-[120px]">
-            <h1 className="font-semibold text-xl text-white tracking-widest text-opacity-90">{title}</h1>
-            <div className="mt-10 grid grid-cols-4 gap-6">
-                {achievementCards.map(card => <Card key={card.name} name={card.name} highestBid={card.highestBid}
-                                                    price={card.price} time={card.time} like={card.like}/>)}
-            </div>
-        </section>
-    )
+const AchievementSection = ({ title, achievementCards }) => {
+  return (
+    <section className="mt-20 px-[120px]">
+      <h1 className="text-xl font-semibold tracking-widest text-white text-opacity-90">{title}</h1>
+      <div className="grid grid-cols-4 gap-6 mt-10">
+        {achievementCards.map((card) => (
+          <Card
+            key={card.name}
+            name={card.name}
+            highestBid={card.highestBid}
+            price={card.price}
+            time={card.time}
+            like={card.like}
+          />
+        ))}
+      </div>
+    </section>
+  )
 }
 
-export {AchievementSection}
+export { AchievementSection }
--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,31 +1,33 @@
-const Card = ({name, highestBid, price, time, like}) => {
-    return (
-        <div className="bg-[#2B2E35] max-w-[282px] h-[404px] rounded-[18px] shadow-md p-3.5">
-            <div className="relative">
-                <img src="png/Cards/img.png" alt="cardImage" className="w-[254px] h-[254px]"/>
-                <div
-                    className="absolute flex content-center top-[10px] left-[10px] rounded-[20px] py-2.5 pl-[14px] pr-[18px] bg-[#2B2E35]">
-                    <img src="svg/Card/clock.svg" alt="clock"/>
-                    <p className="ml-[10px] bg-gradient-to-br from-[#FD6D1D] to-[#E53AD2] bg-clip-text text-[14px] tracking-[0.05em] text-transparent">08:10:00</p>
-                </div>
-                <div className="absolute top-[10px] right-[10px] rounded-[20px] p-2.5 bg-[#2B2E35]">
-                    <img src="svg/Card/heart.svg" alt="heart"/>
-                </div>
-            </div>
-            <div className="mt-5 flex justify-between">
-                <div className="">
-                    <h1 className="text-xl text-white opacity-90">{name}</h1>
-                    <p className="text-sm text-white opacity-60 mt-2">{highestBid}</p>
-                </div>
-                <img src="svg/Card/ethereum.svg" alt="ethereum"/>
-            </div>
-            <div className="mt-5 flex justify-between">
-                    <p className="text-base text-white opacity-90">Price</p>
-                <p className="ml-[10px] bg-gradient-to-br from-[#FD6D1D] font-semibold to-[#E53AD2] bg-clip-text text-base tracking-[0.05em] text-transparent">{price}</p>
-
-            </div>
+const Card = ({ name, highestBid, price, time, like }) => {
+  return (
+    <div className="h-[404px] max-w-[282px] rounded-[18px] bg-[#2B2E35] p-3.5 shadow-md">
+      <div className="relative">
+        <img src="png/Cards/img.png" alt="cardImage" className="h-[254px] w-[254px]" />
+        <div className="absolute top-[10px] left-[10px] flex content-center rounded-[20px] bg-[#2B2E35] py-2.5 pl-[14px] pr-[18px]">
+          <img src="svg/Card/clock.svg" alt="clock" />
+          <p className="ml-[10px] bg-gradient-to-br from-[#FD6D1D] to-[#E53AD2] bg-clip-text text-[14px] tracking-[0.05em] text-transparent">
+            08:10:00
+          </p>
         </div>
-    )
+        <div className="absolute top-[10px] right-[10px] rounded-[20px] bg-[#2B2E35] p-2.5">
+          <img src="svg/Card/heart.svg" alt="heart" />
+        </div>
+      </div>
+      <div className="flex justify-between mt-5">
+        <div className="">
+          <h1 className="text-xl text-white opacity-90">{name}</h1>
+          <p className="mt-2 text-sm text-white opacity-60">{highestBid}</p>
+        </div>
+        <img src="svg/Card/ethereum.svg" alt="ethereum" />
+      </div>
+      <div className="flex justify-between mt-5">
+        <p className="text-base text-white opacity-90">Price</p>
+        <p className="ml-[10px] bg-gradient-to-br from-[#FD6D1D] to-[#E53AD2] bg-clip-text text-base font-semibold tracking-[0.05em] text-transparent">
+          {price}
+        </p>
+      </div>
+    </div>
+  )
 }
 
-export {Card}
+export { Card }
