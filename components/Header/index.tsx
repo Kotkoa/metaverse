e3fa826236eea93a6b3a80966a1164e23267fc3a
Kotkoa <kotkoa@gmail.com>
2022-06-22 10:17:51 +0200

    fix blantic and Logo as next/image

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,14 +1,16 @@
+import Image from 'next/image'
 import Link from 'next/link'
+import Logo from '/public/svg/Header/logo.svg'
 
 const Header = () => {
   return (
     <div className="flex justify-between bg-[#2B2E35] px-32 py-6">
       <Link href="/">
-        <img src="svg/Header/logo.svg" alt="" />
-        {/*<div*/}
-        {/*    className="bg-gradient-to-br from-[#FD6D1D] to-[#E53AD2] bg-clip-text font-blantic text-[32px] tracking-[0.14em] text-white text-transparent">*/}
-        {/*    LOGO*/}
-        {/*</div>*/}
+        <Image src={Logo} alt="LOGO" width={98} height={40} />
+        {/* <div className="bg-gradient-to-br from-[#FD6D1D] to-[#E53AD2] bg-clip-text font-blantic text-[32px] tracking-[0.14em] text-white text-transparent">
+          LOGO
+        </div> */}
+        {/* >>>> check font - is displayed */}
       </Link>
       <button
         className="rounded-[28px] bg-gradient-to-r from-[#FD6D1D] to-[#E53AD2] px-6 py-2.5 tracking-widestmore text-white text-opacity-90"
