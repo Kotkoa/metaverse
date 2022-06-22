975b157781c67404c2ef53d6188de713d9d72e10
Kotkoa <kotkoa@gmail.com>
2022-06-22 12:12:37 +0200

    fix cards center on mobile

--- components/AchievementSection/index.tsx
+++ components/AchievementSection/index.tsx
@@ -4,7 +4,7 @@ const AchievementSection = ({ title, achievementCards }) => {
   return (
     <section className="mt-20 px-[120px]">
       <h1 className="text-xl font-semibold tracking-widest text-white text-opacity-90">{title}</h1>
-      <div className="grid grid-cols-1 gap-6 mt-10 sm:grid-cols-2 lg:grid-cols-4">
+      <div className="grid grid-cols-1 gap-6 mt-10 justify-items-center sm:grid-cols-2 lg:grid-cols-4">
         {achievementCards.map((card) => (
           <Card
             key={card.name}
