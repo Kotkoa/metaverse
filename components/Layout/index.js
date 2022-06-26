f1c356171c455c1ae208e807046a83a447be02e8
Kotkoa <kotkoa@gmail.com>
2022-06-26 13:42:36 +0200

    fix

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,3 +1,10 @@
+import { Header } from '../../components/Header'
+
 export default function Layout({ children }) {
-  return <div>{children}</div>
+  return (
+    <div className="mx-auto max-w-[1440px]">
+      <Header />
+      <div>{children}</div>
+    </div>
+  )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Header } from '../components/Header'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="min-h-screen w-full bg-[#212429]">
-      <div className="mx-auto max-w-[1440px]">
-        <Header />
-        <main>
-          <Component {...pageProps} />
-        </main>
-      </div>
+      <Component {...pageProps} />
     </div>
   )
 }
