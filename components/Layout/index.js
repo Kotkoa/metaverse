fff52adbe2d91d3a8beab7b76c9fb21f98239d8b
Kotkoa <kotkoa@gmail.com>
2022-06-26 13:32:37 +0200

    fix layout and _app

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,14 +1,3 @@
-import { Header } from '../Header'
-
 export default function Layout({ children }) {
-  return (
-    <div className="min-h-screen w-full bg-[#212429]">
-      <div className="mx-auto max-w-[1440px]">
-        <Header />
-        <main>
-          <div className="">{children}</div>
-        </main>
-      </div>
-    </div>
-  )
+  return <div>{children}</div>
 }
